8e76632836ea74e91437d6776c6ac1d83d168842
Faiza1st <faixa.faisal@gmail.com>
2024-09-03 20:34:03 +1000

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import {NavBar} from './navbar.js';
+import {NavBar} from './components/navbar.js';
+import {Banner} from './components/banner.js';
+import { Skills } from './components/skills.js';
+import { Projects } from './components/projects.js';
+import { Footer } from './components/footer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
      <NavBar/>
+     <Banner/>
+     <Skills />
+     <Projects/>
+     <Footer />
 
     </div>
   );
